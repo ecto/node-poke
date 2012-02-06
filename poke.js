52992cac098fcc966c81b382f7a75ae8c621d042
Cam Pedersen <diffference@gmail.com>
2012-02-05 21:20:20 -0500

    initial commit

---
+++ poke.js
@@ -0,0 +1,26 @@
+var mdns = require('mdns');
+var colors = require('colors');
+
+var browsers = [];
+
+var sbrowser = mdns.browseThemAll();
+
+sbrowser.on('serviceUp', function(s) {
+  try {
+    var b = mdns.createBrowser(mdns[s.type.protocol](s.type.name))
+    b.on('serviceUp', logPoint);
+    b.start();
+    browsers.push(b);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+sbrowser.start();
+
+var logPoint = function (p) {
+  console.log('\n\n' + p.name.blue);
+  console.log('  ' + p.type.protocol.grey + ' ' + p.type.name);
+  console.log('  ' + p.addresses[0] + ':' + p.port);
+  console.log('  ' + p.host.grey);
+}
