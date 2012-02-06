d8d3e39cc82eb8dbe934672baa97d5175164919c
Cam Pedersen <diffference@gmail.com>
2012-02-05 21:30:08 -0500

    [major] fix error handling for mdns lib

--- poke.js
+++ poke.js
@@ -1,18 +1,12 @@
 #!/usr/bin/env node
 var mdns = require('mdns');
 var colors = require('colors');
-var browsers = [];
 var sbrowser = mdns.browseThemAll();
 
 sbrowser.on('serviceUp', function(s) {
-  try {
-    var b = mdns.createBrowser(mdns[s.type.protocol](s.type.name))
-    b.on('serviceUp', logPoint);
-    b.start();
-    browsers.push(b);
-  } catch (e) {
-    console.log(e);
-  }
+  var b = mdns.createBrowser(mdns[s.type.protocol](s.type.name))
+  b.on('serviceUp', logPoint);
+  b.start();
 });
 
 sbrowser.start();
@@ -23,3 +17,9 @@ var logPoint = function (p) {
   console.log('  ' + p.addresses[0] + ':' + p.port);
   console.log('  ' + p.host.grey);
 }
+
+// hack for mdns library
+// it correctly doesn't allow service types with
+// names longer than 14 characters, but it's not
+// worth crashing the process for
+process.on('uncaughtException', console.log);
